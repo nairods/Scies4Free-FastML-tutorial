b75bcabe737cdf28a466399269acab667eeeb9e3
nairods <77335960+nairods@users.noreply.github.com>
2026-03-11 09:27:30 +0100

    Refactor all_callbacks class for improved readability

--- callbacks.py
+++ callbacks.py
@@ -68,12 +68,24 @@ class Losstimer(Callback):
 
 class all_callbacks:
     def __init__(
-        self, stop_patience=10, lr_factor=0.5, lr_patience=1, lr_epsilon=0.001, lr_cooldown=4, lr_minimum=1e-5, outputDir=''
+        self,
+        stop_patience=10,
+        lr_factor=0.5,
+        lr_patience=1,
+        lr_epsilon=0.001,
+        lr_cooldown=4,
+        lr_minimum=1e-5,
+        outputDir=''
     ):
         self.nl_begin = newline_callbacks_begin(outputDir)
         self.nl_end = newline_callbacks_end()
 
-        self.stopping = EarlyStopping(monitor='val_loss', patience=stop_patience, verbose=1, mode='min')
+        self.stopping = EarlyStopping(
+            monitor='val_loss',
+            patience=stop_patience,
+            verbose=1,
+            mode='min'
+        )
 
         self.reduce_lr = ReduceLROnPlateau(
             monitor='val_loss',
@@ -81,29 +93,41 @@ class all_callbacks:
             patience=lr_patience,
             mode='min',
             verbose=1,
-            epsilon=lr_epsilon,
+            min_delta=lr_epsilon,
             cooldown=lr_cooldown,
             min_lr=lr_minimum,
         )
 
         self.modelbestcheck = ModelCheckpoint(
-            outputDir + "/KERAS_check_best_model.h5", monitor='val_loss', verbose=1, save_best_only=True
+            outputDir + "/KERAS_check_best_model.keras",
+            monitor='val_loss',
+            verbose=1,
+            save_best_only=True
         )
 
         self.modelbestcheckweights = ModelCheckpoint(
-            outputDir + "/KERAS_check_best_model_weights.h5",
+            outputDir + "/KERAS_check_best_model.weights.h5",
             monitor='val_loss',
             verbose=1,
             save_best_only=True,
             save_weights_only=True,
         )
 
-        self.modelcheckperiod = ModelCheckpoint(outputDir + "/KERAS_check_model_epoch{epoch:02d}.h5", verbose=1, period=10)
+        self.modelcheckperiod = ModelCheckpoint(
+            outputDir + "/KERAS_check_model_epoch{epoch:02d}.keras",
+            verbose=1,
+            save_freq='epoch'
+        )
 
-        self.modelcheck = ModelCheckpoint(outputDir + "/KERAS_check_model_last.h5", verbose=1)
+        self.modelcheck = ModelCheckpoint(
+            outputDir + "/KERAS_check_model_last.keras",
+            verbose=1
+        )
 
         self.modelcheckweights = ModelCheckpoint(
-            outputDir + "/KERAS_check_model_last_weights.h5", verbose=1, save_weights_only=True
+            outputDir + "/KERAS_check_model_last.weights.h5",
+            verbose=1,
+            save_weights_only=True
         )
 
         self.tb = TensorBoard(log_dir=outputDir + '/logs')
